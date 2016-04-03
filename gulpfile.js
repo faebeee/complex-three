5c96ce25993a7bcafa328eaa2e459809adcfddec
Faebeee <faebeee@gmail.com>
2016-04-03 19:37:24 +0200

    add three lib

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   return gulp.src([
+    './lib/**/*.js',
     './src/**/*.js',
     ])
     .pipe(concat('complex-three.js'))
