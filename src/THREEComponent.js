b64c6cd1a3b87951620589d47c9ce2064ea6fadf
fabs <faebeee@gmail.com>
2020-04-12 12:10:00 +0200

    Update doc

--- src/THREEComponent.js
+++ src/THREEComponent.js
@@ -1,8 +1,14 @@
 import { Component } from 'complex-engine';
 
 export default class THREEComponent extends Component {
+    /**
+     * 
+     * @param {THREE.Mesh} mesh 
+     */
     constructor(mesh) {
         super();
+        
+        /** @var {THREE.Mesh} */
         this.mesh = mesh;
     }
 
